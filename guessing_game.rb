43ce4851cda62452edddd87606270e41cfc25097
Sanam Akbar <sanamakbar@Sanams-MacBook-Pro.local>
2015-08-27 11:05:50 -0400

    correct guess prog quits

--- guessing_game.rb
+++ guessing_game.rb
@@ -7,7 +7,9 @@ input = "x"
 number = rand(100)
 puts number
 puts "Guess a number between 0 to 100"
+puts "Try to guess it in 5 tries or less"
 guess = gets.to_i
+tries= 1
 
 if guess < number
   print('Your guess is too low. Guess again.')
@@ -21,5 +23,4 @@ end
 
 if guess == number
   puts "You guessed right!"
-  exit
 end

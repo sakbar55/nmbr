b96eb7a8b12a20954c2acf9980e06a3f95e89aef
Sanam Akbar <akbar.sanam@gmail.com>
2015-09-08 16:12:31 -0400

    final

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,63 +1,35 @@
-# Pick a random number between 1 and 100
-input = "x"
-
-# Prompt you for a guess
-
-
-number = rand(100)
+number = rand(100) + 1
+guess_array = Array.new
 puts number
 puts "Guess a number between 0 to 100"
 puts "Try to guess it in 5 tries or less"
-guess = gets.to_i
-guess_count= guess_count +1
-tries= 1
 
-def check(message)
+
+def input(message,array)
   puts message
+  guess_left(array)
+  input_guess = gets.chomp.to_i
+  array << input_guess
+
+  return input_guess
 end
+
+def guess_left(array)
+  left = array.length
+  puts "Keep trying" unless left == 0
+end
+
+guess = input("Guess a number between 1 & 100.",guess_array)
+while guess_array.length < 5
 
 if guess < number
-  print('Your guess is too low. Guess again.')
-guess= gets.to_i
-end
-
-if guess > number
-  print('Your guess is too high. Guess again.')
-  guess= gets.to_i
-end
-
-if guess == number
+  guess = input("Your guess is too low. Guess again.",guess_array)
+elsif guess > number
+  guess = input("Your guess is too high. Guess again.",guess_array)
+else guess == number
   puts "You guessed right!"
-end
-
-def check(message)
-  puts message
-end
-
-while guess_count <= 5
-  guesses.each do |x|
-    if x == guess
-      check("You suck! Nope. Try again!")
-    end
-  end
-  def winner(message)
-  puts message
-end
-
-def loser(message)
-  puts message
-end
-
-  if guess == number
-  loser("Beginners luck!")
   exit
   end
-  if guess_count == 5
-    puts "You are out of chances. You lose."
-    winner("I win!")
-    exit
 end
-  guesses << guess
-  guess = gets.to_i
-  guess_count = guess_count +1
-end
+
+print "You are out of tries. You lose!"
